a56e4636da129d65c0cb9012d9f2c37ce290a8ea
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2026-05-23 13:28:18 +0330

    new release

--- app/menu/components/FoodGrid.tsx
+++ app/menu/components/FoodGrid.tsx
@@ -3,10 +3,24 @@
 import { memo } from "react";
 import FoodCard from "@/components/FoodCard";
 
-function FoodGrid({ foods, language, onSelectFood, t, getFoodName, getIngredients }: {foods: any; language: string; onSelectFood: any; t: any; getFoodName: any; getIngredients: any}) {
+function FoodGrid({
+  foods,
+  language,
+  onSelectFood,
+  t,
+  getFoodName,
+  getIngredients,
+}: {
+  foods: any;
+  language: string;
+  onSelectFood: any;
+  t: any;
+  getFoodName: any;
+  getIngredients: any;
+}) {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-      {foods.map((food , index) => (
+      {foods.map((food: any, index: number) => (
         <FoodCard
           key={food.id}
           food={food}
