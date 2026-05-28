863dc26aa50bd19f180c4e7f69b264eca23992d2
Ali Ibrahimi <alimadad.ibrahimi1116@gmail.com>
2026-05-29 01:44:17 +0330

    new update

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,6 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
